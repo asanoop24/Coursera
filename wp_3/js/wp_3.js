2cbc710abab49a3a86fad0d7b6444d1533e9b298
asanoop24 <asanoop24@gmail.com>
2016-08-07 20:20:58 +0530

    Added Menu Area - I

--- wp_3/js/wp_3.js
+++ wp_3/js/wp_3.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
+//    $('.menuLine, .menuIcon').addClass('menuInactive');
+    var menuActive = 0;
     function initialize(){
         var options = {
             types:['(regions)'],
             componentRestrictions: {country: "IN"}
-         };
+         }; 
         var input = document.getElementById('locality');
         var autocomplete = new google.maps.places.Autocomplete(input, options);
     }
-    google.maps.event.addDomListener(window, 'load', initialize);
-    document.getElementById('submit').addEventListener('click', function(){
-       alert(document.getElementById('locality').value); 
+    if($('.selectArea').val('')){
+        google.maps.event.addDomListener(window, 'load', initialize);
+        document.getElementById('submit').addEventListener('click', function(){
+            alert(document.getElementById('locality').value);
+        });
+    }
+    $('.selectCity').change(function(){
+        $('.selectArea').val('');
     });
+    
+    
+    $('.menuIcon').click(function(){
+        if($('.menuIcon').hasClass('menuActive')){
+            $('.menuArea, .menuLine, .menuIcon').removeClass('menuActive').hide().show().addClass('menuInactive');
+        }
+        else{
+            $('.menuArea, .menuLine, .menuIcon').removeClass('menuInactive').hide().show().addClass('menuActive');
+        }
+    });
+    
 });
