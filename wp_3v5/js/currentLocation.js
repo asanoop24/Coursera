ca3b74a6011d6436fef8a4d0ace1d60b9d7fe448
asanoop24 <asanoop24@gmail.com>
2016-08-10 21:29:41 +0530

    google map embedded

---
+++ wp_3v5/js/currentLocation.js
@@ -0,0 +1,72 @@
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
+};
+
+      function initMap() {
+          var map = new google.maps.Map(document.getElementById('map'), {
+              center: {lat: 134.397, lng: 150.644},
+              zoom: 15
+          });
+          var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+          if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                  var pos = {
+                      lat: position.coords.latitude,
+                      lng: position.coords.longitude
+                  };
+                  infoWindow.setPosition(pos);
+                  infoWindow.setContent('hun check kar haan');
+                  map.setCenter(pos);
+                  //o chal ja hun parava
+                  var query = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + pos.lat + ',' + pos.lng + '&sensor=true';
+                    document.getElementById('detectLocation').addEventListener('click', function(){
+                        getJSON(query, function(err, data) {
+                            if (err != null) {
+                                alert('Something went wrong: ' + err);
+                            } 
+                            else {
+                               document.getElementById('locality').value = data.results[1].formatted_address;
+                            }
+                        });
+                    });
+
+                  var marker = new google.maps.Marker
+                  (
+                      {
+                          position: new google.maps.LatLng(pos.lat, pos.lng),
+                          map: map,
+                          title: 'Click me'
+                      }
+                  );
+                  
+              }, function() {
+                  handleLocationError(true, infoWindow, map.getCenter());
+              });
+          }
+          
+          else {
+          // Browser doesn't support Geolocation
+              handleLocationError(false, infoWindow, map.getCenter());
+          }
+      }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.'
+                         );
+}
---
+++ wp_3v5/js/location.js
@@ -0,0 +1,58 @@
+function initMap(){
+    var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13
+    });
+
+    var input = document.getElementById('pac-input');
+    var types = 'Addresses';
+    
+    var autoComplete = new google.maps.places.Autocomplete(input);
+    var infoWindow = new google.maps.infoWindow();
+    var marker = new google.maps.marker({
+        map : map,
+        anchorPoint : new google.maps.Point(0, -29);
+    });
+    
+    autoComplete.addListener('place_changed', function(){
+        infoWindow.close();
+        marker.setVisible(false);
+        var place = autoComplete.getPlace();
+        
+        //    1. locating the point on map
+        if(!place.geometry){
+            window.alert('Autocomplete place contains no geometry!');
+        }
+        
+        if(place.geometry.viewport){
+            map.fitBounds(place.geometry.viewport);
+        }
+        else{
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+        
+        
+        //    2. setting up the marker
+        marker.setIcon(({
+            url: place.icon,
+            size: new google.maps.Point(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+        }));
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+        
+        var address = '';
+        if(place.address_components){
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || ''),
+            ].join(' ');
+        }
+        
+        infoWindow.setContent('<div><strong>' + place.name + '<strong><br>' + address);
+        infoWindow.open(map, marker);
+});
