83cf1067c51affd511b12d7a8861a703de17da26
asanoop24 <asanoop24@gmail.com>
2016-08-10 13:49:32 +0530

    google map embedded

---
+++ wp_3v4/js/wp_3.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+//    $('.menuLine, .menuIcon').addClass('menuInactive');
+    var menuActive = 0;
+    function initialize(){
+        var options = {
+            types:['(regions)'],
+            componentRestrictions: {country: "IN"}
+         }; 
+        var input = document.getElementById('locality');
+        var autocomplete = new google.maps.places.Autocomplete(input, options);
+    }
+    if($('.selectArea').val('')){
+        google.maps.event.addDomListener(window, 'load', initialize);
+        document.getElementById('submitText').addEventListener('click', function(){
+            alert(document.getElementById('locality').value);
+        });
+    }
+    $('.selectCity').change(function(){
+        $('.selectArea').val('');
+    });
+    
+    
+    $('.menuIcon').click(function(){
+//        alert('click to ho rha hai');
+        if($('.menuIcon').hasClass('menuActive')){
+            $('.menuArea, .menuLine, .menuIcon').removeClass('menuActive').hide().show().addClass('menuInactive');
+        }
+        else{
+            $('.menuArea, .menuLine, .menuIcon').removeClass('menuInactive').hide().show().addClass('menuActive');
+        }
+    });
+    
+});
