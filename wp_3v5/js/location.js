c4fb63c3aca68d333c1f4c267ee64780dcf20ab3
asanoop24 <asanoop24@gmail.com>
2016-08-11 02:47:36 +0530

    google map embedded

--- wp_3v5/js/location.js
+++ wp_3v5/js/location.js
@@ -45,6 +45,7 @@ function initMap(){
 //    Set position through marker movement    
     google.maps.event.addListener(marker, 'dragend', function(){
         var pos = marker.getPosition();
+        alert(pos[0] + ' - ' + pos[1]);
         var query = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + pos.lat + ',' + pos.lng + '&sensor=true';
         getJSON(query, function(err, data){
             document.getElementById('locality').value = data.results[1].formatted_address;
