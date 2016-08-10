f21d31924ed4d2164306d78534ca02841a981ff7
asanoop24 <asanoop24@gmail.com>
2016-08-10 17:42:37 +0530

    google map embedded

--- wp_3v4/js/currentLocation.js
+++ wp_3v4/js/currentLocation.js
@@ -1,35 +1,62 @@
-function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 8,
-          center: {lat: 40.731, lng: -73.997}
-        });
-        var geocoder = new google.maps.Geocoder;
-        var infowindow = new google.maps.InfoWindow;
+ 
+      function initMap() {
+          var map = new google.maps.Map(document.getElementById('map'), {
+              center: {lat: 134.397, lng: 150.644},
+              zoom: 15
+          });
+          var infoWindow = new google.maps.InfoWindow({map: map});
 
-        $(document).ready(function() {
-          geocodeLatLng(geocoder, map, infowindow);
-        });
-      }
-
-      function geocodeLatLng(geocoder, map, infowindow) {
-        var input = document.getElementById('latlng').value;
-        var latlngStr = input.split(',', 2);
-        var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-        geocoder.geocode({'location': latlng}, function(results, status) {
-          if (status === 'OK') {
-            if (results[1]) {
-              map.setZoom(11);
-              var marker = new google.maps.Marker({
-                position: latlng,
-                map: map
+        // Try HTML5 geolocation.
+          if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                  var pos = {
+                      lat: position.coords.latitude,
+                      lng: position.coords.longitude
+                  };
+                  infoWindow.setPosition(pos);
+//                  infoWindow.setContent('hun ki haal ne');
+                  map.setCenter(pos);
+//                  alert(pos.lat + ' - ' + pos.lng);
+                  //o chal ja hun parava
+                  function geocodeLatLng(geocoder){
+                      geocoder.geocode({'location': latlng}, function(results, status) {
+                          if (status === 'OK') {
+                              if (results[1]) {
+                                  map.setZoom(15);
+                                  alert(results[1].formatted_address);
+                                  infowindow.setContent(results[1].formatted_address);
+                              } else {
+                                  window.alert('No results found');
+                              }                      
+                          }
+                          else {
+                              window.alert('Geocoder failed due to: ' + status);
+                          }
+                      }
+                  var marker = new google.maps.Marker
+                  (
+                      {
+                          position: new google.maps.LatLng(pos.lat, pos.lng),
+                          map: map,
+                          title: 'Click me'
+                      }
+                  );
+                  
+              }, function() {
+                  handleLocationError(true, infoWindow, map.getCenter());
               });
-              infowindow.setContent(results[1].formatted_address);
-              infowindow.open(map, marker);
-            } else {
-              window.alert('No results found');
-            }
-          } else {
-            window.alert('Geocoder failed due to: ' + status);
           }
-        });
+          
+          else {
+          // Browser doesn't support Geolocation
+              handleLocationError(false, infoWindow, map.getCenter());
+          }
       }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.'
+                         );
+}
