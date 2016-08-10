9c44d6afb831ab68b704c6fa700a2253dd660c5c
asanoop24 <asanoop24@gmail.com>
2016-08-10 16:12:42 +0530

    google map embedded

--- wp_3v4/js/currentLocation.js
+++ wp_3v4/js/currentLocation.js
@@ -19,7 +19,7 @@
                   var marker = new google.maps.Marker
                   (
                       {
-                          position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                          position: new google.maps.LatLng(pos),
                           map: map,
                           title: 'Click me'
                       }
