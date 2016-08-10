b931cae0842deb140e13d291c4fc8988d4268e0a
asanoop24 <asanoop24@gmail.com>
2016-08-11 00:31:25 +0530

    google map embedded

--- wp_3v5/js/location.js
+++ wp_3v5/js/location.js
@@ -41,8 +41,8 @@ function initMap(){
                 var query = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + pos.lat + ',' + pos.long + '&sensor=true';
                 getJSON(query, function(err, data){
                     document.getElementById('locality').value = data.results[1].formatted_address;
-                    map.setCenter(pos);
-                    marker.setPosition(pos);
+                    map.setCenter(pos.lat, pos.long);
+                    marker.setPosition(pos.lat, pos.long);
                     marker.setVisible(true);
                 });
             });
